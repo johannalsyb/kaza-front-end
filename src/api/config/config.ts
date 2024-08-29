93b10b08a65d14aeb34ecdc80b41d9936f1494c3
Arthur Itey <itey.arthur@gmail.com>
2024-08-29 16:38:58 +0100

    fix(env): change api url

--- src/api/config/config.ts
+++ src/api/config/config.ts
@@ -1,3 +1,7 @@
-export const API_URL = process.env.REACT_APP_API_URL || "/api"
-export const WS_URL = process.env.REACT_APP_WS_URL ||
-    `${document.location.protocol === "https:" ? "wss" : "ws"}://${document.location.host}${API_URL}`
+export const API_URL = 'https://api.kazaswap.co';
+// export const API_URL = process.env.REACT_APP_API_URL || "/api"
+export const WS_URL =
+  process.env.REACT_APP_WS_URL ||
+  `${document.location.protocol === 'https:' ? 'wss' : 'ws'}://${
+    document.location.host
+  }${API_URL}`;
