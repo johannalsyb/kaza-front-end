f929a9afda0a9329072546eeaf5e0455f5508b88
johannalsyb <79452051+johannalsyb@users.noreply.github.com>
2025-05-17 15:29:31 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const SentryWebpackPlugin = require("@sentry/webpack-plugin");
+
 module.exports = {
   module: {
     rules: [
@@ -9,9 +11,20 @@ module.exports = {
       {
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
-        resourceQuery: {not: [/url/]}, // exclude react component if *.svg?url
+        resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
         use: ['@svgr/webpack'],
       },
     ],
   },
+
+  plugins: [
+    new SentryWebpackPlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: "kaza-swap",
+      project: "kaza-front-end",
+      release: "kaza-front-end@1.0.0",
+      include: "./build", // or your actual output folder
+      ignore: ["node_modules", "webpack.config.js"],
+    }),
+  ],
 };
