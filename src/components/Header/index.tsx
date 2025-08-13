ad2d5cb14687cc52fe161563f59f3e47eac02cb5
hassanjaved <flancer@saldri.com>
2025-08-13 15:12:26 +0500

    added credits showing in component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -318,7 +318,7 @@ export default (
                 stroke: variables.colors.blackLight,
               }}
             />
-            <Text style={{ fontFamily: "Plus Jakarta Sans", fontSize: 18, fontWeight: '500' }}>{5}</Text>
+            <Text style={{ fontFamily: "Plus Jakarta Sans", fontSize: 18, fontWeight: '500' }}>{user?.credits}</Text>
           </Pressable>}
         </>
       ) : (
@@ -406,7 +406,7 @@ export default (
                           stroke: variables.colors.blackLight,
                         }}
                       />
-                      <Text style={{ fontSize: 16, marginLeft: -2 }}>{5}</Text>
+                      <Text style={{ fontSize: 16, marginLeft: -2 }}>{user?.credits}</Text>
                     </Pressable>
                     <View
                       ref={popupMenuRef}
